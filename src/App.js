fa7680af31560298ba5c474776011e6f96be6117
Matiss Rolavs <matissrolavs2005@gmail.com>
2023-11-01 18:20:49 +0200

    added loading

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 import ToDo from "./ToDo.js"
 import {useEffect, useState} from "react";
+
 function App() {
   
   const [todo, setTodo] = useState();
+  const [isLoading, setIsLoading] = useState(true); // Add a loading state
   
   useEffect(()=>{
     async function getTodo(){
-      const response = await fetch("https://jsonplaceholder.typicode.com/todos/69");
+      const response = await fetch("https://jsonplaceholder.typicode.com/todos/90");
       const data = await response.json();
       setTodo(data);
-      }
-      getTodo();
-    }, []);
+      setIsLoading(false); // Set loading to false once data is fetched
+    }
+    getTodo();
+  }, []);
 
-  
-  // const todo = {
-  //   userId: 1,
-  //   id: 1,
-  //   title: "delectus aut autem",
-  //   completed: false,
-  // };
-  
-  
-
+  // Only render ToDo once data is fetched
   return (
     <>
-      <ToDo {...todo}/>
+      {isLoading ? 'Loading...' : <ToDo {...todo}/>}
     </>
   );
 }
